59f0cec8ddb5234df5280e5dfff425057bfcc4c3
acid03160407 <acid03160407@gmail.com>
2025-02-16 16:56:38 +0900

    リレーション修正・コメント数・ライク数表示

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable
     }
 
     public function subjects(){
-        return $this->belongsToMany('App\Models\Users\subjects','subject_users','user_id','subject_id');// リレーションの定義 訂正
+        return $this->belongsToMany('App\Models\Users\Subjects','subject_users','user_id','subject_id');// リレーションの定義 訂正
     }
 
     // いいねしているかどうか
